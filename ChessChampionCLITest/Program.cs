3e77267c9b1f2089f182daba935542a2d850b40e
Markus Nisula <markus_nisula@hotmail.com>
2021-05-12 01:05:15 +0300

    Add project files.

---
+++ ChessChampionCLITest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChessChampionCLITest
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var startInfo = new ProcessStartInfo(@"C:\Users\Mark\Downloads\stockfish_13_win_x64\stockfish_13_win_x64\stockfish_13_win_x64.exe");
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false;
+
+            Process process = new()
+            {
+                StartInfo = startInfo
+            };
+            process.Start();
+
+            StreamWriter streamWriter = process.StandardInput;
+            StreamReader streamReader = process.StandardOutput;
+
+            string moves = "position startpos moves e2e4";
+            await WriteMessage(streamWriter, moves);
+
+            await WriteMessage(streamWriter, "go");
+
+            await Task.Delay(2000);
+
+            await WriteMessage(streamWriter, "stop");
+
+            await ReadResponse(streamReader);
+
+            Console.ReadLine();
+        }
+
+        private static async Task WriteMessage(StreamWriter streamReader, string message)
+        {
+            await streamReader.WriteLineAsync(message);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+
+        private static async Task ReadResponse(StreamReader streamReader)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            char[] buffer = new char[4096];
+            int x = await streamReader.ReadAsync(buffer);
+            string response = new string(buffer);
+            Console.WriteLine(response);
+        }
+    }
+}
